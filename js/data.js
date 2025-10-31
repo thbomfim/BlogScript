993811df7cca8ea34ee31ecde83413e359d98e05
thbomfim <thbomfim01@gmail.com>
2025-10-31 20:09:19 -0300

    tirei o post generico e coloquei um realmente

--- js/data.js
+++ js/data.js
@@ -1,16 +1,9 @@
 export const posts = [
         {   
             id: 1,
-            titulo: "Começando com JavaScript",
-            conteudo: "Este e meu primeiro post neste renomado e humilde Blog!",
-            data: "17/10/2025",
-            autor: "Thiago Bomfim"
-        },
-        {
-            id: 2,
-            titulo: "Segundo Post",
-            conteudo: "Eiusmod consequat officia ea reprehenderit minim veniam. Nisi veniam et proident cillum et nisi laborum. Aute labore officia est eu nisi officia et enim culpa sunt nostrud enim. Laborum reprehenderit irure mollit duis exercitation laborum veniam. Nulla magna laboris esse fugiat amet quis anim dolor laborum labore duis irure sint. Tempor nisi elit amet ea. Duis exercitation velit incididunt occaecat aliquip laborum duis nostrud nisi labore.",
-            data: "18/10/2025",
+            titulo: 'Meu primeiro Post',
+            conteudo: '<p>Olá meu nome é thiago este é meu primeiro post nesse blog, talvez você caiu aqui sem querer então aqui vai um resumo do porque estou criando esse blog:</p>O principal motivo foi para começar estudar JavaScript (é uma linguagem de programação), talvez em um futuro este blog nao seja tão simples quanto é hoje na data de criação deste post 31/10/2025 ou talvez vai continuar isso o tempo dirar, aliais por falar em tempo talvez este seja um segundo motivo de eu ter criado um blog primeiro para aprender javaScript e nao um to do list da vida pois com blog eu posso literalmente acompanhar minha evolução pessoal e tambem minha mudança de digamos filosofica da vida, não espere os melhores textos aqui sem falhas gramaticas não vou usar nem mesmo IA para fazer correções gramaticais quero ver minha evolução até mesmo na escrita no mais até a proxima e caso queira da uma olhada este blog esta no meu repositorio do github <a href="https://github.com/thbomfim/BlogScript" target="_blank" rel="noopener noreferrer">Clique aqui e de uma olhada</a> no mais agradeço por ter lido até aqui, se quiser falar comigo me mande mensagem no telegram com o @thbomfim Até mais 👍🏻',
+            data: "31/10/2025",
             autor: "Thiago Bomfim"
         }
     ];
